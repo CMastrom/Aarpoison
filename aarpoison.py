2a9df326faee81e4138523fa897fd5ce0a38e0a8
cmastromonaco <70275549+cmastromonaco@users.noreply.github.com>
2021-01-26 23:11:14 -0600

    Update

--- aarpoison.py
+++ aarpoison.py
@@ -1,7 +1,62 @@
-from os import popen
+from subprocess import check_output
 from colors import Msg
 from sys import argv
 from sys import exit as sysexit
+from time import sleep
+from signal import signal, SIGINT
+frozen_ips = []
+
+def handler(signal_received, frame):
+    # Handle any cleanup here
+    print('Ctrl + C detected, unfreezing targeted ips...')
+    try:
+        for ip in frozen_ips:
+            print(Msg.fnote("UNFREEZING CONNECTION")+ " Attempting to create a dynamic entry for "+ip+" in ARP table...")
+            if if_dynamic(ip):
+                print(Msg.ferror("ERROR")+" Cannot set entry to dynamic because it already is static.")
+                sysexit(1)
+            print(Msg.fnote("DELETE STATIC")+" Attempting to delete the old static entry...")
+            print(popen("sudo arp -d "+ip))
+            print(Msg.fnote("DYNAMIC ADD")+" Attempting to add dynamic entry:")
+            print(popen("ping "+ip+" -c 5"))
+            if if_dynamic(ip):
+                print(Msg.fsucc("SUCCESS")+" Router entry in ARP table is dynamic (unfrozen)!")
+            else:
+                print(Msg.ferror("ERROR")+" Router entry failed to update to dynamic in ARP table")
+    except Exception as e:
+        print(e)
+        sysexit(0)
+    sysexit(0)
+signal(SIGINT, handler)
+
+def popen(command):
+    process = check_output(command.split(" "))
+    try:
+        process = process.decode('utf-8').split("\n")
+    except Exception as e:
+        print(Msg.ferror("POPEN ERROR")+":")
+        print(e)
+    return process
+
+def if_permanent(router):
+    arp_table_lines = popen("arp -a")
+    for line in arp_table_lines:
+        l = line.split(" ")
+        if l[1].lstrip("(").rstrip(")") == router:
+            if l[6] == "permanent":
+                return True
+            return False
+    return False
+
+def if_dynamic(router):
+    arp_table_lines = popen("arp -a")
+    for line in arp_table_lines:
+        l = line.split(" ")
+        if l[1].lstrip("(").rstrip(")") == router:
+            if l[6] == "ifscope":
+                return True
+            return False
+    return False
 
 try:
     if argv[1] == "--defend":
@@ -12,11 +67,73 @@ try:
             Options:
                 [ --help ] displays the current message
                 [ --defend ] enables defensive mode, which enables aarpoison, in the event of a discovered ARP Poisoning attack, to try and counter the attack
+                [ --frz-conn $router_ip ] sets arp table entry for the router (or any ip) to static (NOTE: if the router doesn't have this device as a static entry in its ARP table, then someone can still ARP poison the router! This only prevents ARP poisoning of your device!)
+                [ --unfrz-conn $router_ip ] sets arp table entry for the router (or any ip) to dynamic
         """)
+        sysexit(1)
+    elif argv[1] == "--frz-conn":
+        try:
+            print(Msg.fnote("FREEZING CONNECTION")+ " Attempting to create a static entry for "+argv[2]+" in ARP table...")
+            router = argv[2]
+            arp_table_lines = popen("arp -a")
+            for line in arp_table_lines:
+                l = line.split(" ")
+                if l[1].lstrip("(").rstrip(")") == router:
+                    print(Msg.fnote("FOUND ROUTER")+" The following commands may require a password (they need to be run as sudo)...")
+                    if if_permanent(router):
+                        print(Msg.ferror("ERROR")+" Cannot set entry to static because it already is static.")
+                        sysexit(1)
+                    print(Msg.fnote("STATIC ADD")+" Attempting to add static entry:")
+                    print(popen("sudo arp -s "+l[1].lstrip("(").rstrip(")")+" "+l[3]))
+                    print(Msg.fnote("DELETE DYNAMIC")+" Attempting to delete the old dynamic entry...")
+                    print(popen("sudo arp -d "+l[1].lstrip("(").rstrip(")")))
+                    if if_permanent(router):
+                        print(Msg.fsucc("SUCCESS")+" Router entry in ARP table is static (frozen)!")
+                    else:
+                        print(Msg.ferror("ERROR")+" Router entry failed to update to static in ARP table")
+                    break
+        except Exception as e:
+            try: 
+                router = argv[2]
+                print(e)
+            except Exception as error:
+                print(Msg.ferror("FAILED")+" Must provide the router ip after specifying --freeze-connection")
+                print(error)
+            sysexit(1)
+        sysexit(1)
+    elif argv[1] == "--unfrz-conn":
+        try:
+            print(Msg.fnote("UNFREEZING CONNECTION")+ " Attempting to create a dynamic entry for "+argv[2]+" in ARP table...")
+            router = argv[2]
+            arp_table_lines = popen("arp -a")
+            for line in arp_table_lines:
+                l = line.split(" ")
+                if l[1].lstrip("(").rstrip(")") == router:
+                    print(Msg.fnote("FOUND ROUTER")+" The following commands may require a password (they need to be run as sudo)...")
+                    if if_dynamic(router):
+                        print(Msg.ferror("ERROR")+" Cannot set entry to dynamic because it already is static.")
+                        sysexit(1)
+                    print(Msg.fnote("DELETE STATIC")+" Attempting to delete the old static entry...")
+                    print(popen("sudo arp -d "+l[1].lstrip("(").rstrip(")")))
+                    print(Msg.fnote("DYNAMIC ADD")+" Attempting to add dynamic entry:")
+                    print(popen("ping "+l[1].lstrip("(").rstrip(")")+" -c 5"))
+                    if if_dynamic(router):
+                        print(Msg.fsucc("SUCCESS")+" Router entry in ARP table is dynamic (unfrozen)!")
+                    else:
+                        print(Msg.ferror("ERROR")+" Router entry failed to update to dynamic in ARP table")
+                    break
+        except Exception as e:
+            try: 
+                router = argv[2]
+                print(e)
+            except Exception as error:
+                print(Msg.ferror("FAILED")+" Must provide the router ip after specifying --unfreeze-connection")
+                print(error)
+            sysexit(1)
+        sysexit(1)
     else:
         print(Msg.ferror("INVALID PARAM")+ " Parameter  '"+argv[1]+"' unrecognized.")
         sysexit(1)
-
 except Exception:
     DEFEND = False
 
@@ -32,15 +149,6 @@ def reset():
     }
 reset()
 
-# def load_arp_table():
-#     global ips, macs
-#     reset()
-#     for (mac,ip) in zip(ips, macs):
-#         arp_table["ip"].append(ip)
-#         arp_table["mac"].append(mac)
-#     ips.clear()
-#     macs.clear()
-
 def find_duplicate(mac_addr):
     global arp_table
     ip_found_at = []
@@ -51,11 +159,14 @@ def find_duplicate(mac_addr):
 
 def parse_arp_table(line, index):
     l = line.split(" ")
+    if not l or l[0] is None or l[0] == "":
+        return
     try:
         ips.append(l[1].lstrip("(").rstrip(")"))
         macs.append(l[3])
-    except Exception:
+    except Exception as e: 
         print(Msg.ferror("Failed to parse row: "+line))
+        raise e
     # Compare arp table to current arp table
     try:
         if arp_table["ip"][index] != l[1].lstrip("(").rstrip(")"):
@@ -66,7 +177,6 @@ def parse_arp_table(line, index):
             arp_table["ip"][index] = l[1].lstrip("(").rstrip(")")
         if arp_table["mac"][index] != l[3]:
             print(Msg.ferror("POSSIBLE ARP POISON: "+arp_table["mac"][index]+" DOES NOT MATCH "+l[3]))
-            print("\t"+Msg.fwarn("POTENTIAL ATTACKER: ")+ "Potential attacker ip: "+l[1].lstrip("(").rstrip(")"))
             # Try to find attacker:
             pot_attacker_ip = find_duplicate(l[3])
             attacker_ip = "[Failed to find Attacker IP]"
@@ -74,17 +184,19 @@ def parse_arp_table(line, index):
                 if aip != arp_table["ip"][index]:
                     attacker_ip = aip
                     break
+            print("\t"+Msg.fwarn("POTENTIAL ATTACKER: ")+ "Potential attacker ip: "+attacker_ip)
             print("\t"+Msg.fcyan("SWITCHED")+ arp_table["ip"][index] + " switched to mac "+l[3]+" by IP "+attacker_ip)
             if DEFEND:
-                # Try and reset the MAC to the correct one
-                print("\t"+Msg.fnote("RESETTING")+ " Resetting ARP Poising device to correct mac address...")
-                print("\t"+Msg.fnote("DUMPING RESPONSE:"))
-                print(popen("arp -s "+arp_table["ip"][index]+" "+arp_table["mac"][index]+"; ping "+arp_table["ip"][index]))
-                print("\t"+Msg.fcyan("SWITCHED (DEFEND)")+ arp_table["ip"][index] + " switched back to mac "+arp_table['mac'][index]+" by SELF")
-                # Do not update arp_table because it was reset back
+                if arp_table['ip'][index] not in frozen_ips:
+                    # Try and reset the MAC to the correct one
+                    print("\t"+Msg.fnote("RESETTING")+ " Resetting ARP Poising device to correct mac address...")
+                    print("\t"+Msg.fnote("DUMPING RESPONSE:"))
+                    print(popen("sudo arp -s "+arp_table["ip"][index]+" "+arp_table["mac"][index]+"; sudo arp -d "+arp_table["ip"][index]))
+                    print("\t"+Msg.fcyan("SWITCHED (DEFEND)")+ arp_table["ip"][index] + " switched back to mac "+arp_table['mac'][index]+" by SELF")
+                    print("\t"+Msg.fwarn("FROZE ATTACKED IP ENTRY")+" To prevent further attacks, "+arp_table['ip'][index]+" entry has been frozen (set to permanent) to mac address "+arp_table['mac'][index])
+                    frozen_ips.append(arp_table['ip'][index])
+                    # Do not update arp_table because it was reset back
             else:
-                #dc:ef:9:96:9b:68
-                #bc:ec:23:ca:a6:b
                 # Update arp_table
                 arp_table["mac"][index] = l[3]
     except IndexError:
